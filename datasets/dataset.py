965afe0cc7d09fc4201fd4767f7d7c19a58f8719
TrungQuocNguyen <trungng28296@gmail.com>
2021-10-29 13:29:06 +0200

    Small changes to datasets

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,20 +1,21 @@
 import os
 import numpy as np 
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision import transforms
 from PIL import Image
 from torchvision.transforms.functional import InterpolationMode
 import torchvision.transforms.functional as tf
 import random 
 class ScanNet2D(Dataset): 
-    def __init__(self, img_dir, label_dir, img_size, is_transform = True): 
+    def __init__(self, img_dir, label_dir, img_size, is_transform = True, augmentation = True): 
         self.img_dir = img_dir
         self.label_dir =label_dir
         self.img_list = sorted(os.listdir(img_dir))
         self.label_list = sorted(os.listdir(label_dir))
         self.img_size = img_size
         self.is_transform = is_transform
+        self.augmentation = augmentation
     def __len__(self): 
         return len(self.img_list)
     def __getitem__(self, idx): 
@@ -24,37 +25,38 @@ class ScanNet2D(Dataset):
         img = Image.open(img_name)
 
         label_name = os.path.join(self.label_dir, self.label_list[idx])
-        mask = Image.open(label_name)
+        target = Image.open(label_name)
 
         if self.is_transform: 
-            img, mask  = self.transform(img, mask)
-        return img, mask
-    def transform(self, img, mask):
+            img, target  = self.transform(img, target)
+        return img, target
+    def transform(self, img, target):
         #resize
         #random crop
         #to tensor
         #flipping
 
         resize_img = transforms.Resize(self.img_size, interpolation = InterpolationMode.BILINEAR)
-        resize_mask = transforms.Resize(self.img_size, interpolation = InterpolationMode.NEAREST)
+        resize_target = transforms.Resize(self.img_size, interpolation = InterpolationMode.NEAREST)
         totensor = transforms.ToTensor()
 
         img = resize_img(img)
-        mask = resize_mask(mask)
+        target = resize_target(target)
 
-        if random.random() > 0.5: 
-            img = tf.vflip(img)
-            mask = tf.vflip(mask)
+        if self.augmentation: 
+            if random.random() > 0.5: 
+                img = tf.vflip(img)
+                target = tf.vflip(target)
 
-        if random.random() > 0.5: 
-            img = tf.hflip(img)
-            mask = tf.hflip(mask)
+            if random.random() > 0.5: 
+                img = tf.hflip(img)
+                target = tf.hflip(target)
     
 
         img = totensor(img)
-        mask = np.array(mask, dtype = np.int64)
-        mask = torch.from_numpy(mask)
+        target = np.array(target, dtype = np.int64)
+        target = torch.from_numpy(target)
 
-        return img, mask
+        return img, target
         
 
