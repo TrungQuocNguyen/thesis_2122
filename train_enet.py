36a3f927647e0c433c38dd2badcf3c4d6118d36f
TrungQuocNguyen <trungng28296@gmail.com>
2021-12-09 07:58:35 +0100

    Update

--- train_enet.py
+++ train_enet.py
@@ -38,7 +38,7 @@ def print_params(model):
 
 if __name__ =='__main__': 
     parser = argparse.ArgumentParser(description='PyTorch Training')
-    parser.add_argument('-c', '--config', default='train_config.json',type=str,
+    parser.add_argument('-c', '--config', default='train_enet_config.json',type=str,
                         help='Path to the config file (default: config.json)')
     args = parser.parse_args()
     config = json.load(open(args.config))
